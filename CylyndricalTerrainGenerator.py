90b253c66cc59d9c92aa6aba9296754f998a21b7
Daniel Holland <36774525+Simcrafter@users.noreply.github.com>
2020-09-22 21:24:46 -0700

    wrote some code generates random set of doublewide points

--- CylyndricalTerrainGenerator.py
+++ CylyndricalTerrainGenerator.py
@@ -5,3 +5,26 @@ from scipy.spatial import Voronoi, voronoi_plot_2d
 
 circumfrence = 4000
 height = circumfrence / 2
+
+pList = []
+
+
+pointFactor = 250
+jitterFactor = 75
+for i in range(int(circumfrence / pointFactor)):
+    for j in range(int(height / pointFactor)):
+        nPoint = [ i * pointFactor + (rand.randrange(-jitterFactor, jitterFactor)), 
+        j * pointFactor + (rand.randrange(-jitterFactor, jitterFactor))]
+
+        nPointCirc = [nPoint [0] + circumfrence, nPoint[1]]
+
+        pList.append(nPoint)
+        pList.append(nPointCirc)
+
+
+points = np.array(pList)
+
+vor = Voronoi(points)
+
+plot = voronoi_plot_2d(vor)
+plt.show()
